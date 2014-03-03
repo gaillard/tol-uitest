43785bbbcbdcee521d6155ba7a45897918f71f78
Jonathan Gaillard <jonathan.gaillard@dominionenterprises.com>
2014-03-03 17:14:14 -0500

    Change default domain to m.cycletrader.com since mobile dot isn't working and will presumably go away.

--- settings.js
+++ settings.js
@@ -2,4 +2,4 @@ var _ = require('underscore');
 var system = require('system');
 
 var settings = {domain: system.env.DOMAIN};
-module.exports = _.defaults(settings, {domain: 'http://mobile.cycletrader.com'});
+module.exports = _.defaults(settings, {domain: 'http://m.cycletrader.com'});
